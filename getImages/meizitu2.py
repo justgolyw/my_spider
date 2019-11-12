0d3aad39ff9e42dce38e34e405910d043f6e5f1e
justgolyw <2531222095@qq.com>
2019-11-12 21:41:29 +0800

    renew getImages/meizitu2.py

--- getImages/meizitu2.py
+++ getImages/meizitu2.py
@@ -41,6 +41,7 @@ def get_img():
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.235',
         'Referer': 'https://www.mzitu.com'
     }
+    os.makedirs("Images",exist_ok=True)
 
     # proxies = get_random_ip(get_ip_list())
     response = requests.get(_url, headers=header)
